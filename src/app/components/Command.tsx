667cf2fe2aead5075687ff6481aa133bc62d9065
Richard Nemeth <nemethricsi@gmail.com>
2025-12-13 22:57:11 +0100

    fix popover and command primitives

--- src/app/components/Command.tsx
+++ src/app/components/Command.tsx
@@ -109,8 +109,7 @@ function CommandItem({
     <CommandPrimitive.Item
       data-slot="command-item"
       className={cn(
-        // 'data-[selected=true]:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground px-2 py-1.5',
-        "data-[selected=true]:bg-fuego-100 relative flex cursor-default items-center gap-2 rounded-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "relative flex cursor-default items-center gap-2 rounded-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 data-[selected=true]:bg-neutral-100 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className,
       )}
       {...props}
